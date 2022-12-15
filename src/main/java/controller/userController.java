7b804cc3dd04893edbec01be119d631e9fcb02ea
Meok Khem <113187567+KhemMeok@users.noreply.github.com>
2022-12-15 23:50:32 +0700

    first commit

---
+++ src/main/java/controller/userController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = "/user")
+public class userController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+		// redirct 
+		
+		request.getRequestDispatcher("WEB-INF/view/welcome.jsp").forward(request, response);
+		
+		
+		/*		 System.out.println("work");
+		PrintWriter writer = response.getWriter();
+		String html = """
+                <html>
+                    <body>\
+                        <p>Hello, '\s' World</p>\
+                    </body>
+                </html>
+                """;
+		writer.write(html);
+		*/
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+    <table>
+        <tr>
+
+            <td><label for="text">Name:</label></td>
+            <td><input type="text" name="name" id="name"></td>
+
+        </tr>
+        <tr>
+            <td>
+                <label for="text">Password</label>
+            </td>
+            <td> <input type="text" name="pass" id="pass"></td>
+        </tr>
+        <tr>
+            <td>
+                <button>login</button>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
